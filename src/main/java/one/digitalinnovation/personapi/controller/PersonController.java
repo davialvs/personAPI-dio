8c6c6fd2b5bc29139e8ee11da8c786cf542050bb
Davi Alves <72519831+davialvs@users.noreply.github.com>
2020-12-08 18:00:28 -0300

    Developed person create operation

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.personapi.controller;
+
+
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/people")
+public class PersonController {
+
+        private PersonService personService;
+
+        @Autowired
+        public PersonController(PersonService personService) {
+            this.personService = personService;
+        }
+
+        @PostMapping
+        @ResponseStatus(HttpStatus.CREATED)
+        public MessageResponseDTO createPerson(@RequestBody Person person) {
+            return personService.createPerson(person);
+        }
+}
