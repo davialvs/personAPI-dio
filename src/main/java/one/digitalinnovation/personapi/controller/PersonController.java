674f2f4791f4efd7892f1819f7808336d7b75ae7
Davi Alves <72519831+davialvs@users.noreply.github.com>
2020-12-08 19:05:51 -0300

    Improved person creation functionality

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,6 +1,7 @@
 package one.digitalinnovation.personapi.controller;
 
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.service.PersonService;
@@ -21,7 +22,7 @@ public class PersonController {
 
         @PostMapping
         @ResponseStatus(HttpStatus.CREATED)
-        public MessageResponseDTO createPerson(@RequestBody Person person) {
-            return personService.createPerson(person);
+        public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+            return personService.createPerson(personDTO);
         }
 }
